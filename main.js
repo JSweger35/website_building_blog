a70543ad495ef8474a8ef07b3ff125d48c95e155
Jake Sweger <jsweg13@gmail.com>
2023-04-14 14:13:52 -0400

    Update Status for day 1.

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const container = document.querySelector('#container');
 
 
-const paragraph = document.createElement('p');
+const paragraph = document.createElement('div');
 paragraph.classList.add('paragraph');
-paragraph.textContent = 'This is a test text.';
+paragraph.textContent = "This is day one of creating my own website. This is the journey of working toward becoming a software engineer/developer. I'm looking forward to the journey.";
 container.appendChild(paragraph);
 
-console.log("Hello World!")
+
+
