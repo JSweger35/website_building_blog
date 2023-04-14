1b4ee7fc832f72d0e0493c82c8fc7c478409fc76
Jake Sweger <jsweg13@gmail.com>
2023-04-14 13:47:14 -0400

    Add test text to server

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const container = document.querySelector('#container');
 
-const content = document.createElement('div');
+/*const content = document.createElement('div');
 content.classList.add('content');
+content.style.color = 'white';
 content.textContent = 'This is the glorious text-content!';
-container.appendChild(content);
+container.appendChild(content);*/
 
 const paragraph = document.createElement('p');
 paragraph.classList.add('paragraph');
-paragraph.style.color = 'red';
-paragraph.textContent = 'Hi I\'m red!';
+paragraph.style.color = 'white';
+paragraph.textContent = 'This is a test text.';
 container.appendChild(paragraph);
 
 console.log("Hello World!")
