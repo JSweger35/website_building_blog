de8c6c964426b1062f1223a475ad9097f50801a0
Jake Sweger <jsweg13@gmail.com>
2023-04-14 13:57:53 -0400

    Fix text color

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
 const container = document.querySelector('#container');
 
-/*const content = document.createElement('div');
-content.classList.add('content');
-content.style.color = 'white';
-content.textContent = 'This is the glorious text-content!';
-container.appendChild(content);*/
 
 const paragraph = document.createElement('p');
 paragraph.classList.add('paragraph');
-paragraph.style.color = 'white';
 paragraph.textContent = 'This is a test text.';
 container.appendChild(paragraph);
 
