343bd7c1bf32fcd27b442c7cd36ca342630cc7e2
Jake Sweger <jsweg13@gmail.com>
2023-04-17 09:11:58 -0400

    Add a card look to the content section.

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const container = document.querySelector('#container');
 
 const paragraph = document.createElement('div');
 paragraph.classList.add('paragraph');
+paragraph.textContent = "This is day two of creating my own  website. I'm adding a card style approach to the pag and whatever else feels right after that."
+container.appendChild(paragraph);
+paragraph.classList.add('paragraph');
 paragraph.textContent = "This is day one of creating my own website. This is the journey of working toward becoming a software engineer/developer. I'm looking forward to the journey.";
 container.appendChild(paragraph);
 
