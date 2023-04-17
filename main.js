f62c609cf0a37acdce3cfc101683ec67ac1aa16b
Jake Sweger <jsweg13@gmail.com>
2023-04-17 09:30:42 -0400

    Add second day info. Change flex direction

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const container = document.querySelector('#container');
 
 
-const paragraph = document.createElement('div');
-paragraph.classList.add('paragraph');
-paragraph.textContent = "This is day two of creating my own  website. I'm adding a card style approach to the pag and whatever else feels right after that."
-container.appendChild(paragraph);
-paragraph.classList.add('paragraph');
-paragraph.textContent = "This is day one of creating my own website. This is the journey of working toward becoming a software engineer/developer. I'm looking forward to the journey.";
-container.appendChild(paragraph);
+const p1 = document.createElement('div');
+p1.classList.add('p1');
+p1.textContent = "This is day two of creating my own  website. I'm adding a card style approach to the pag and whatever else feels right after that."
+container.appendChild(p1);
+
+const p2 = document.createElement('div');
+p2.classList.add('p2');
+p2.textContent = "This is day one of creating my own website. This is the journey of working toward becoming a software engineer/developer. I'm looking forward to the journey.";
+container.appendChild(p2);
 
 
 
