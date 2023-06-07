6f35f25bbc734f509e569d6c4289c543809228ee
Jake Sweger <jsweg13@gmail.com>
2023-06-07 15:00:37 -0400

    Link js and stylsheet

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const container = document.querySelector('#container');
 
+const p3 = document.createElement('div');
+p3.classList.add('p3');
+p3.textContent = "This is day three of creating my own website. ";
+container.appendChild(p3);
 
 const p1 = document.createElement('div');
 p1.classList.add('p1');
@@ -10,6 +14,8 @@ const p2 = document.createElement('div');
 p2.classList.add('p2');
 p2.textContent = "This is day one of creating my own website. This is the journey of working toward becoming a software engineer/developer. I'm looking forward to the journey.";
 container.appendChild(p2);
+
+
 
 
 
